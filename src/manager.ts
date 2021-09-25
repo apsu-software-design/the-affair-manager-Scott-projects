44ce09430074a7cd2b389ad236ef02809303a544
Scott <slam@my.apsu.edu>
2021-09-24 22:43:07 -0500

    Added comments and README

--- src/manager.ts
+++ src/manager.ts
@@ -1,6 +1,8 @@
 import { stringify } from "querystring";
 import { listenerCount } from "stream";
 
+// Scott Lam
+// CSCI 4602 - Assignment 2
 import { Member } from "./member";
 import { Affair } from "./affair";
 import { Organization } from "./organization";
@@ -64,6 +66,10 @@ export class AffairManager {
     }
 
     public modifyAffair(affairName: string, newTitle: string, newTime?: any) { //Doesn't work
+        //For some reason the console says the setAffairName and setDate or undefined and I can't figure it out
+        //I was trying first get the index of the name and find that in the affairList
+        //Then, it should get the obj of that affair and set the name/time to what the user wants
+        //I'm guessing it doesn't get the affair object exactly how I want it to be
         var affairIndex = this.affairList.findIndex(x => x.name == affairName);
 
         this.affairList[affairIndex].setAffairName(newTitle); //Undefined
@@ -80,10 +86,15 @@ export class AffairManager {
 
     }
 
-    public getMembers(affairName: string): string[] {
+    public getMembers(affairName: string): string[] { //Doesn't work
+        //Similar to the ModifyAffair, I tried finding the name using the findName function which
+        //helps find the affair object in the list
+        //(Was testing a few ways to get the affairName in the list (None worked))
+        //I had to put a 0 there or else it would give me an error (Worked in grabbing the first item but still error)
+        //I know there is definitely something wrong with affObj but I couldn't it figure out 
         var affObj: Affair[] = this.findName(affairName, this.affairList);
 
-        return affObj[0].getMembersInvolved();
+        return affObj[0].getMembersInvolved(); //Undefined
 
 
 
